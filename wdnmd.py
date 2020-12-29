5a98eac827ae7a836a6b357019b5b35ed53397ed
Hrz <296730738@qq.com>
2020-12-29 19:28:30 +0800

    23

--- wdnmd.py
+++ wdnmd.py
@@ -1,1 +1,15 @@
+def upstairs(num):
+    if num==1:
+        return 1
+    if num==2:
+        return 2
+    else:
+        return upstairs(num-1)+upstairs(num-2)
 print("WDNMD")
+num = input("请输入台阶级数:")
+try:
+    num = int(num)
+    steps = upstairs(num) 
+    print("一共有",steps,"种方法上台阶")
+except Exception as e:
+    print("不是数字")
